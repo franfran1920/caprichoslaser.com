52fb6081a352ec8bcb100be357aba962e1994776
root <root@caprichoslaser@gmail.com>
2023-08-24 18:46:48 +0000

    Actualizado wordpress y plugins

--- wp-content/jetpack-waf/bootstrap.php
+++ wp-content/jetpack-waf/bootstrap.php
@@ -5,5 +5,5 @@ define( 'JETPACK_WAF_MODE', 'normal' );
 define( 'JETPACK_WAF_SHARE_DATA', '1' );
 define( 'JETPACK_WAF_DIR', '/var/www/html/caprichoslaser.com/wp-content/jetpack-waf' );
 define( 'JETPACK_WAF_WPCONFIG', '/var/www/html/caprichoslaser.com/wp-content/../wp-config.php' );
-require_once '/var/www/html/caprichoslaser.com/wp-content/plugins/jetpack-disabled/vendor/autoload.php';
+require_once '/var/www/html/caprichoslaser.com/wp-content/plugins/jetpack/vendor/autoload.php';
 Automattic\Jetpack\Waf\Waf_Runner::initialize();
